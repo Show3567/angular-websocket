0c51f4d235f2bbc542adaa1fee26da0e35a35185
Yue Dong <ydong011@fiu.edu>
2022-09-11 10:01:07 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+const config: SocketIoConfig = {
+  url: 'http://localhost:4232',
+  options: {},
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SocketIoModule.forRoot(config)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
